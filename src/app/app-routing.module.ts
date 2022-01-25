b66083ab647972eda40e16dbe06c51d41087e99c
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2022-01-25 13:23:37 +0100

    one more link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventsListComponent } from './page/events-list/events-list.component';
+import { EventDetailsComponent } from './page/event-details/event-details.component';
 
 const routes: Routes = [
-  {path: '', component: EventsListComponent}
+  {path: '', component: EventsListComponent},
+  {path: 'details', component: EventDetailsComponent}
 ];
 
 @NgModule({
